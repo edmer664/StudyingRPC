d0856c5836b5c9579eac3bf23dad71cb12ba2acb
Edmerson Pizarra <edmerpizarra@gmail.com>
2021-11-09 09:58:15 +0800

    ADD: motivational quotes

--- study.py
+++ study.py
@@ -1,6 +1,8 @@
 from pypresence import Presence
 import time
 from itertools import cycle
+import requests
+import os
 
 client_id = "902536560539484202"
 
@@ -13,10 +15,17 @@ lines = [
     "PLEASE DO NOT",
     "DISTURB, THANKS!",
 ]
-            
+r = requests.get("https://type.fit/api/quotes")
+response = r.json()
+
+
 while 1:
-    for i in cycle(lines):
-        RPC.update(small_image="book",large_image="book", details="I'm studying",
-            state=i,
-            start=start_time)
-        time.sleep(3) 
+    for q in response:
+        os.system("cls")
+        print("MOTIVATE YOUR SELF \n\n\n")
+        print(f'''"{q["text"]}"''',"\n -",q["author"])
+        for i in lines:
+            RPC.update(small_image="book",large_image="book", details="I'm studying",
+                state=i,
+                start=start_time)
+            time.sleep(2) 
